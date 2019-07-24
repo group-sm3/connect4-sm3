ec1030ddb7a428228a74a0a3154540c24b9deb6c
dal79 <dal79@users.noreply.github.com>
2019-07-24 15:22:43 -0500

    Create test package
    added test package for JUnit tests

---
+++ src/test/BoardTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import view.Board;
+
+class BoardTest {
+
+	Board board;
+
+	@Test
+	void checkColumnSelcetions() {
+		fail("Not yet implemented");
+	}
+
+}
