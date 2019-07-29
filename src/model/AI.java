de65be3cd7113fc65cf313b6ece8f656aafc994f
KellDrew <38505535+KellDrew@users.noreply.github.com>
2019-07-29 00:08:48 -0500

    Add files via upload

---
+++ src/model/AI.java
@@ -0,0 +1,559 @@
+package model;
+
+public class AI {
+private int botColor;
+private int playerColor;
+
+public void SetColor(int color) 
+{
+	this.botColor = color;
+	if(botColor == 1)
+	{
+		playerColor = 2;
+	}
+	else 
+	{
+		playerColor = 1;
+	}
+}
+
+public int BotTurn(int [][] grid, int counter[])
+{
+	int bestCol = 0; //optimal position, to be returned
+	int score[] = {2,2,2,2,2,2,2}; //score of each column
+	//CHECK FOR THREE IN A ROW
+	//check columns
+	for (int i = 0; i < 7; i++)
+	{
+		int y = counter[i]; //height determined by piece count
+		try {
+			if(grid[i][y+1] == grid[i][y+2] && grid[i][y+2] == grid[i][y+3]) 
+			{
+				if(grid[i][y+1] == botColor)
+				{
+					score[i] = 7;
+				}
+				if(grid[i][y+1] == playerColor)
+				{
+					score[i] = 6;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+	}
+	//check rows
+	for(int i = 0; i < 7; i++)
+	{
+		int y = counter[i];
+		try {
+		if(grid[i + 1][y] == grid[i + 2][y] && grid[i + 2][y] == grid[i + 3][y])
+        {
+            //Debug.Log("a case 1");
+            if(grid[i + 1][y] == playerColor)
+            {
+                if(score[i] < 5)
+                {
+                    score[i] = 6;
+                }
+            }
+            if(grid[i+1][y] == botColor)
+            {
+                score[i] = 7;
+            }
+        }
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+        if (grid[i - 1][y] == grid[i - 2][y] && grid[i - 2][y] == grid[i - 3][y])
+        {
+            //Debug.Log("a case 2");
+            if (grid[i - 1][y] == playerColor)
+            {
+
+                if (score[i] < 6)
+                {
+                    score[i] = 6;
+                }
+            }
+            if (grid[i - 1][y] == botColor)
+            {
+                score[i] = 7;
+            }
+        }
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+        if (grid[i - 2][y] == grid[i - 1][y] && grid[i - 1][y] == grid[i + 1][y])
+        {
+            //Debug.Log("a case 3");
+            if (grid[i + 1][y] == playerColor)
+            {
+                if (score[i] < 6)
+                {
+                    score[i] = 6;
+                }
+            }
+            if (grid[i + 1][y] == botColor)
+            {
+                score[i] = 7;
+            }
+        }
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+        if (grid[i - 1][y] == grid[i + 1][y] && grid[i + 1][y] == grid[i + 2][y])
+        {
+            //Debug.Log("a case 4");
+            if (grid[i + 1][y] == playerColor)
+            {
+                if (score[i] < 6)
+                {
+                    score[i] = 6;
+                }
+            }
+            if (grid[i + 1][y] == botColor)
+            {
+                score[i] = 7;
+            }
+        }
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+	
+	//check diagonals
+	try {
+	if (grid[i - 3][y - 3] == grid[i - 2][y - 2] && grid[i - 2][y - 2] == grid[i - 1][y - 1])
+    {
+        //Debug.Log("a case 6");
+        if (grid[i - 3][y - 3] == botColor)
+        {
+            score[i] = 7;
+        }
+        else if (grid[i - 3][y - 3] == playerColor && score[i] < 6)
+        {
+            score[i] = 6;
+        }
+    }}
+    catch(ArrayIndexOutOfBoundsException exception) {		}
+    //seek upper right
+	try {
+    if (grid[i - 2][y - 2] == grid[i - 1][y - 1] && grid[i - 1][y - 1] == grid[i + 1][y + 1])
+    {
+        //Debug.Log("a case 7");
+        if (grid[i - 2][y - 2] == playerColor && score[i] < 6)
+        {
+            score[i] = 6;
+        }
+        else if (grid[i - 2][y - 2] == botColor)
+        {
+            score[i] = 7;
+        }
+    }}
+    catch(ArrayIndexOutOfBoundsException exception) {		}
+    //seek lower left
+	try {
+    if (grid[i - 1][y - 1] == grid[i + 1][y + 1] && grid[i + 1][y + 1] == grid[i + 2][y + 2])
+    {
+        //Debug.Log("a case 8");
+        if (grid[i - 1][y - 1] == playerColor && score[i] < 6)
+        {
+            score[i] = 6;
+        }
+        else if (grid[i - 1][y - 1] == botColor)
+        {
+            score[i] = 7;
+        }
+    }}
+    catch(ArrayIndexOutOfBoundsException exception) {		}
+    //seek lower right
+	try {
+    if (grid[i + 1][y + 1] == grid[i + 2][y + 2] && grid[i + 2][y + 2] == grid[i + 3][y + 3])
+    {
+        //Debug.Log("a case 9");
+        if (grid[i + 1][y + 1] == playerColor && score[i] < 6)
+        {
+            score[i] = 6;
+        }
+        else if (grid[i + 1][y + 1] == botColor)
+        {
+            score[i] = 7;
+        }
+    }}
+    catch(ArrayIndexOutOfBoundsException exception) {		}
+    //seek partial right upper
+	try {
+    if (grid[i - 3][y + 3] == grid[i - 2][y + 2] && grid[i - 2][y + 2] == grid[i - 1][y + 1])
+    {
+        //Debug.Log("a case 10");
+        if (grid[i - 3][y + 3] == playerColor && score[i] < 6)
+        {
+            score[i] = 6;
+        }
+        else if (grid[i - 3][y + 3] == botColor)
+        {
+            score[i] = 7;
+        }
+    }}
+    catch(ArrayIndexOutOfBoundsException exception) {		}
+    //seek partial right lower
+	try {
+    if (grid[i - 2][y + 2] == grid[i - 1][y + 1] && grid[i - 1][y + 1] == grid[i + 1][y - 1])
+    {
+       // Debug.Log("a case 11");
+        if (grid[i - 2][y + 2] == playerColor && score[i] < 6)
+        {
+            score[i] = 6;
+        }
+        else if (grid[i - 2][y + 2] == botColor)
+        {
+            score[i] = 7;
+        }
+    }}
+    catch(ArrayIndexOutOfBoundsException exception) {		}
+    //seek partial left upper
+	try {
+    if (grid[i - 1][y + 1] == grid[i + 1][y - 1] && grid[i + 1][y - 1] == grid[i + 2][y - 2])
+    {
+        //Debug.Log("a case 12");
+        if (grid[i - 1][y + 1] == playerColor && score[i] < 6)
+        {
+            score[i] = 6;
+        }
+        else if (grid[i - 1][y + 1] == botColor)
+        {
+            score[i] = 7;
+        }
+    }}
+    catch(ArrayIndexOutOfBoundsException exception) {		}
+    //seek partial left lower
+	try {
+    if (grid[i + 1][y - 1] == grid[i + 2][y - 2] && grid[i + 2][y - 2] == grid[i + 3][y - 3])
+    {
+        //Debug.Log("a case 13");
+        if (grid[i + 1][y - 1] == playerColor && score[i] < 6)
+        {
+            score[i] = 6;
+        }
+        else if (grid[i + 1][y - 1] == botColor)
+        {
+            score[i] = 7;
+        }
+    }}
+    catch(ArrayIndexOutOfBoundsException exception) {		}
+	}
+	//CHECK FOR 2 IN A ROW
+	for(int i = 0; i < 7; i++)
+	{
+		int y = counter[i];
+	//by column
+		try {
+			if(grid[i][y+1] == grid[i][y+2]) 
+			{
+				if(grid[i][y+1] == botColor && score[i] < 5)
+				{
+					score[i] = 5;
+				}
+				if(grid[i][y+1] == playerColor && score[i] < 4)
+				{
+					score[i] = 4;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+	//by row
+		try {
+			if(grid[i- 2][y] == grid[i - 1][y]) 
+			{
+				if(grid[i - 1][y] == botColor && score[i] < 5)
+				{
+					score[i] = 5;
+				}
+				if(grid[i - 1][y] == playerColor && score[i] < 4)
+				{
+					score[i] = 4;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+			if(grid[i - 1][y] == grid[i + 1][y]) 
+			{
+				if(grid[i - 1][y] == botColor && score[i] < 5)
+				{
+					score[i] = 5;
+				}
+				if(grid[i - 1][y] == playerColor && score[i] < 4)
+				{
+					score[i] = 4;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+			if(grid[i + 1][y] == grid[i + 2][y]) 
+			{
+				if(grid[i + 1][y] == botColor && score[i] < 5)
+				{
+					score[i] = 5;
+				}
+				if(grid[i + 1][y] == playerColor && score[i] < 4)
+				{
+					score[i] = 4;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+	//by diagonal
+		try {
+			if(grid[i - 2][y - 2] == grid[i - 1][y - 1]) 
+			{
+				if(grid[i - 1][y - 1] == botColor && score[i] < 5)
+				{
+					score[i] = 5;
+				}
+				if(grid[i - 1][y - 1] == playerColor && score[i] < 4)
+				{
+					score[i] = 4;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+			if(grid[i - 1][y - 1] == grid[i + 1][y + 1]) 
+			{
+				if(grid[i - 1][y - 1] == botColor && score[i] < 5)
+				{
+					score[i] = 5;
+				}
+				if(grid[i - 1][y - 1] == playerColor && score[i] < 4)
+				{
+					score[i] = 4;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+			if(grid[i + 1][y + 1] == grid[i + 2][y + 2]) 
+			{
+				if(grid[i + 1][y + 1] == botColor && score[i] < 5)
+				{
+					score[i] = 5;
+				}
+				if(grid[i + 1][y + 1] == playerColor && score[i] < 4)
+				{
+					score[i] = 4;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+			if(grid[i - 2][y + 2] == grid[i - 1][y + 1]) 
+			{
+				if(grid[i - 1][y + 1] == botColor && score[i] < 5)
+				{
+					score[i] = 5;
+				}
+				if(grid[i - 1][y + 1] == playerColor && score[i] < 4)
+				{
+					score[i] = 4;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+			if(grid[i - 1][y + 1] == grid[i + 1][y - 1]) 
+			{
+				if(grid[i - 1][y + 1] == botColor && score[i] < 5)
+				{
+					score[i] = 5;
+				}
+				if(grid[i - 1][y + 1] == playerColor && score[i] < 4)
+				{
+					score[i] = 4;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+			if(grid[i + 1][y - 1] == grid[i + 2][y - 2]) 
+			{
+				if(grid[i + 1][y - 1] == botColor && score[i] < 5)
+				{
+					score[i] = 5;
+				}
+				if(grid[i + 1][y - 1] == playerColor && score[i] < 4)
+				{
+					score[i] = 4;
+				}
+			}
+		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+	}
+	//Else, place piece next to another
+	for(int i = 0; i < 7; i++)
+	{
+		int y = counter[i];
+		try {
+		if (grid[i - 1][y] != 0) //left
+        {
+            //Debug.Log("c case 1");
+            if (score[i] < 3)
+            { score[i] = 3; }
+        }		}
+	    catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+		if (grid[i + 1][y] != 0) //left
+        {
+            //Debug.Log("c case 1");
+            if (score[i] < 3)
+            { score[i] = 3; }
+        }		}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+		if (grid[i][y + 1] != 0) //left
+        {
+            //Debug.Log("c case 1");
+            if (score[i] < 3)
+            { score[i] = 3; }
+        }		}
+    	catch(ArrayIndexOutOfBoundsException exception) {		}
+	}
+	
+	for (int i = 0; i < 7; i++) //check for full column
+	{
+		if(counter[i] < 0)
+		{
+			score[i] = 0; //cannot place here
+		}
+	}
+	//Check if placement would set up player for a winning move
+	for (int i = 0; i < 7; i++)
+	{
+		int y = counter[i] - 1;
+		//row
+		try {
+        if(grid[i-3][y] == grid[i-2][y] && grid[i-2][y] == grid[i-1][y] && grid[i-1][y] == playerColor)
+        {
+            if (score[i] < 7)
+            {
+                score[i] = 1;
+            }
+        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+	        if(grid[i-2][y] == grid[i-1][y] && grid[i-1][y] == grid[i+1][y] && grid[i-1][y] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+	        if(grid[i-1][y] == grid[i+1][y] && grid[i+1][y] == grid[i+2][y] && grid[i-1][y] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+	        if(grid[i+1][y] == grid[i+2][y] && grid[i+2][y] == grid[i+3][y] && grid[i+1][y] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		//diagonal
+		try {
+	        if(grid[i-3][y-3] == grid[i-2][y-2] && grid[i-2][y-2] == grid[i-1][y-1] && grid[i-1][y-1] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+	        if(grid[i-2][y-2] == grid[i-1][y-1] && grid[i-1][y-1] == grid[i+1][y+1] && grid[i+1][y+1] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+	        if(grid[i-1][y-1] == grid[i+1][y+1] && grid[i+1][y+1] == grid[i+2][y+2] && grid[i+1][y+1] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+	        if(grid[i+1][y+1] == grid[i+2][y+2] && grid[i+2][y+2] == grid[i+3][y+3] && grid[i+1][y+1] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+	        if(grid[i-3][y+3] == grid[i-2][y+2] && grid[i-2][y+2] == grid[i-1][y+1] && grid[i-1][y+1] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+	        if(grid[i-2][y+2] == grid[i-1][y+1] && grid[i-1][y+1] == grid[i+1][y-1] && grid[i+1][y-1] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+	        if(grid[i-1][y+1] == grid[i+1][y-1] && grid[i+1][y-1] == grid[i+2][y-2] && grid[i+1][y-1] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+		try {
+	        if(grid[i+1][y-1] == grid[i+2][y-2] && grid[i+2][y-2] == grid[i+3][y-3] && grid[i+1][y-1] == playerColor)
+	        {
+	            if (score[i] < 7)
+	            {
+	                score[i] = 1;
+	            }
+	        }}
+		catch(ArrayIndexOutOfBoundsException exception) {		}
+	}
+	//get highest scoring column
+	int max = 0;
+	for (int i = 0; i < 7; i++)
+	{
+		if(score[i] > score[max])
+		{
+			max = i;
+		}
+	}
+
+	bestCol = max;
+	System.out.println("Scores: " + score[0] + " "+ score[1] + " "+ score[2] + " "+ score[3] + " "+ score[4] + " "+ score[5] + " "+ score[6] + "\n");
+	
+return bestCol;
+}
+
+}
--- src/view/Board.java
+++ src/view/Board.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.swing.ImageIcon;
+
 import javafx.animation.TranslateTransition;
 import javafx.geometry.Insets;
 import javafx.scene.*;
@@ -13,12 +15,13 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Shape;
 import javafx.util.Duration;
+import model.AI;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
 import javafx.scene.effect.Lighting;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.*;
 
 
 /*
@@ -30,10 +33,15 @@ public class Board {
 	private final int COLUMNS = 7;
 	private final int ROWS = 6;
 	private boolean redMove = true;
-	private Disc[][] grid = new Disc[COLUMNS][ROWS];
+	private int[][] grid = new int[COLUMNS][ROWS];
 	private Pane discPane = new Pane();
+	private int counter[] = {5,5,5,5,5,5,5};//counter counts how many free spaces are left (includes 0)
+	public Boolean gameOver = false;
+	private AI bot;
 	
 	private Shape makeGrid() {
+		this.bot = new AI();
+		this.bot.SetColor(2);
     	Shape shape = new Rectangle((COLUMNS + 1) * TILE_SIZE, (ROWS + 1) * TILE_SIZE);
     	
     	for (int y = 0; y < ROWS; y++) 	{
@@ -64,52 +72,93 @@ public class Board {
 			rect.setOnMouseExited(e -> rect.setFill(Color.TRANSPARENT));
 			
 			final int column = x;
-			rect.setOnMouseClicked(e -> placeDisc(new Disc(redMove), column));
-
+			rect.setOnMouseClicked(e -> placeDisc(column));
 			list.add(rect);
     	}
     	
     	return list;
 	}
-
+/*
 	private Optional<Disc> getDisc(int column, int row) {
 		if (column < 0 || column >= COLUMNS || row < 0 || row >= ROWS) {
 			return Optional.empty();
 		}
 		return Optional.ofNullable(grid[column][row]);
-	}
+	}*/
 
-	private void placeDisc(Disc disc, int column) {
-		
-		//Checks next available slot in column
-		int row = ROWS - 1;
-        do {
-            if (!getDisc(column, row).isPresent())
-                break;
-            row--;
-        } while (row >= 0);
-
-        if (row < 0)
-            return;
-
-		//adds disc to column
-        grid[column][row] = disc;
+	private void placeDisc(int column) {
+		if(counter[column] < 0)//if column is full
+		{
+			return;
+		}
+		Disc disc = new Disc(redMove);
+		redMove = !redMove;
+		if(redMove)
+		{
+	        grid[column][counter[column]] = 2; //yellow
+		}
+		else
+		{
+	        grid[column][counter[column]] = 1; //red
+		}
         discPane.getChildren().add(disc);
         disc.setTranslateX(column * (TILE_SIZE + 5) + TILE_SIZE / 4);
-
-        final int currentRow = row; //for gameover condition
 		
 		//Animation
 		TranslateTransition animation = new TranslateTransition(Duration.seconds(0.5), disc);
-        animation.setToY(row * (TILE_SIZE + 5) + TILE_SIZE / 4);
+        animation.setToY(counter[column] * (TILE_SIZE + 5) + TILE_SIZE / 4);
         animation.setOnFinished(e -> {
 			///check GameOver Condition
-            redMove = !redMove;
         });
+        gameOver = CheckWinCondition(column, counter[column]); //if player won, gameOver will be true
+        if(gameOver) {GameEnd(grid[column][counter[column]]);}
+        counter[column]--; //column has one less space
+        AIturn();
         animation.play();
     }
+	
+	private void AIturn()
+	{
+		int column = bot.BotTurn(grid, counter); //best column to place piece
+		Disc disc = new Disc(redMove);
+		redMove = !redMove;
+		if(redMove)
+		{
+	        grid[column][counter[column]] = 2; //yellow
+		}
+		else
+		{
+	        grid[column][counter[column]] = 1; //red
+		}
+        discPane.getChildren().add(disc);
+        disc.setTranslateX(column * (TILE_SIZE + 5) + TILE_SIZE / 4);
+		
+		//Animation
+		TranslateTransition animation = new TranslateTransition(Duration.seconds(0.5), disc);
+        animation.setToY(counter[column] * (TILE_SIZE + 5) + TILE_SIZE / 4);
+        animation.setOnFinished(e -> {
+			///check GameOver Condition
+        });
+        gameOver = CheckWinCondition(column, counter[column]); //if player won, gameOver will be true
+        if(gameOver) {GameEnd(grid[column][counter[column]]);}
+        counter[column]--; //column has one less space
+        animation.play();
+	}
 
-
+	
+	private void GameEnd(int winner) //called on win condition true
+	{
+		//todo: make an actual end game screen and ui which will be displayed now
+		if (winner == 1)
+		{
+		System.out.println("Red wins the game");	
+		}
+		else 
+		{
+			System.out.println("Yellow wins the game");	
+		}
+	}
+	
 	private class Disc extends Circle {
         private final boolean red;
         public Disc(boolean red) {
@@ -120,6 +169,94 @@ public class Board {
 		}
 	}
 	
+	public Boolean CheckWinCondition(int x, int y) //signals: false = no win, true = win
+	{							
+		//x and y indicate where the last piece was placed, check for local win condition
+		int count = 0;
+		for (int i = 0; i < 6; i++)		//check rows
+		{
+			if (grid[i][y] == grid[i + 1][y] && grid[i][y] != 0) 
+			{
+				count++;
+			}
+			else
+			{
+				count = 0;
+			}
+			if(count == 3)
+			{
+				System.out.println("win by row");
+				return true;
+			}
+		}
+		count = 0;
+		for (int i = 0; i < 5; i++) 	//check column
+		{
+			if (grid[x][i] == grid[x][i+1] && grid[x][i] != 0) 
+			{
+				count++;
+			}
+			else {
+				count = 0;
+			}
+			if(count == 3)
+			{
+				System.out.println("win by col");
+				return true;
+			}
+		}
+		count = 0;
+		for (int i = -3; i < 4; i++) 		//check diagonal left
+		{
+			try
+			{
+				if(grid[x+i][y+i] == grid[x+i+1][y+i+1]) {
+					if(grid[x+i][y+i] != 0)
+					{
+						count++;
+					}
+				}
+				else 
+				{	count = 0;	}
+				
+				if(count == 3)
+				{
+					System.out.println("win by l diag");
+					return true;
+				}
+			    }
+		    catch(ArrayIndexOutOfBoundsException exception) {
+				//System.out.println("Error here");
+			}
+		}
+		count = 0;
+		for (int i = -3; i < 4; i++) 		//check diagonal right
+		{
+			try
+			{
+				if(grid[x+i][y-i] == grid[x+i+1][y-i-1]){
+					if(grid[x+i][y-i] != 0)
+					{
+						//System.out.println("hit");
+						count++;
+					}
+				}
+				else 
+				{	count = 0;	}
+				
+				if(count == 3)
+				{
+					System.out.println("win by r diag");
+					return true;
+				}
+			    }
+		    catch(ArrayIndexOutOfBoundsException exception) {
+				//System.out.println("Error here");
+			}
+		}
+		return false; //else, no win
+	}
+	
 	public Pane createBoardPane() {
 		//create board pane
 		Pane board = new Pane();
@@ -128,14 +265,11 @@ public class Board {
     	Shape gridShape = makeGrid();
 		board.getChildren().add(gridShape);
         board.getChildren().addAll(makeColumns());    
-		
-		//loads background image
-		ImageView image = new ImageView(new Image(("res/game_background1.jpg")));
 
 		//combines board with background
 		StackPane spane = new StackPane();
 		spane.setMargin(board, new Insets(60, 160, 40, 160));
-		spane.getChildren().addAll(image, board);
+		spane.getChildren().addAll(board);
 
 		//creates textfield 
 		TextField gameinfo = new TextField();
@@ -148,4 +282,11 @@ public class Board {
     	return vbox;
 	}
 
+	private void PrintGrid() //function to print the grid top to bottom, grid is inverted
+	{
+	for (int i = 0; i < 6; i++) {
+		System.out.print(grid[0][i] + " " + grid[1][i] + " " + grid[2][i] + " " + grid[3][i] + " " + grid[4][i] + " " + grid[5][i] + " " + grid[6][i]);
+		System.out.print("\n");
+	}
+	}
 }
